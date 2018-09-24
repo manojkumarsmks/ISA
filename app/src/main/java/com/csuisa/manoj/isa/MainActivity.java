c7ff5fa2f30576791a16b44478bf81800e04bad4
manojkumarsmks <manojkumarsmks@gmail.com>
2018-09-23 18:13:31 -0600

    Take to FromIndia activity clicking on Things to pack

--- app/src/main/java/com/csuisa/manoj/isa/MainActivity.java
+++ app/src/main/java/com/csuisa/manoj/isa/MainActivity.java
@@ -1,13 +1,27 @@
 package com.csuisa.manoj.isa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout thingsToGet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        thingsToGet = (LinearLayout)findViewById(R.id.things_to_pack);
+
+        thingsToGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FromIndia.class);
+                startActivity(intent);
+            }
+        });
     }
 }
