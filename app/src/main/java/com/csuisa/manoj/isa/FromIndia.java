34eff84c8c8348463d774bde54863747617a560a
manojkumarsmks <manojkumarsmks@gmail.com>
2018-09-23 18:12:20 -0600

    Activity for swipeable layout

---
+++ app/src/main/java/com/csuisa/manoj/isa/FromIndia.java
@@ -0,0 +1,18 @@
+package com.csuisa.manoj.isa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class FromIndia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tabs_to_pack);
+
+
+    }
+}
